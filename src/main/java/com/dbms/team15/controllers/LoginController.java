a714d2e0713c31c72147c27b56f17009eeb91969
DEEP MEHTA <deep.mm@somaiya.edu>
2022-11-04 16:12:39 +0000

    Add login api

---
+++ src/main/java/com/dbms/team15/controllers/LoginController.java
@@ -0,0 +1,64 @@
+package com.dbms.team15.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dbms.team15.models.Customer;
+import com.dbms.team15.models.Employee;
+import com.dbms.team15.models.Login;
+
+@RestController
+public class LoginController {
+
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
+
+  @PostMapping("/api/login")
+  public Login login(@RequestBody Login login) {
+    if (checkAdmin(login.getUsername(), login.getPassword())) {
+        login.setRole("Admin");
+        return login;
+    }
+
+    String sql = "SELECT * FROM EMPLOYEE WHERE USERNAME = '" + login.getUsername() + "' AND PASSWORD = '" + login.getPassword() + "'";
+
+    List<Employee> employees = jdbcTemplate.query(
+      sql,
+      BeanPropertyRowMapper.newInstance(Employee.class)
+    );
+
+    if (employees.size() > 0) {
+        login.setRole("Employee_" + employees.get(0).getROLE());
+        return login;
+    }
+
+    sql = "SELECT * FROM CUSTOMER WHERE USERNAME = '" + login.getUsername() + "' AND PASSWORD = '" + login.getPassword() + "'";
+
+    List<Customer> customers = jdbcTemplate.query(
+      sql,
+      BeanPropertyRowMapper.newInstance(Customer.class)
+    );
+
+    if (customers.size() > 0) {
+        login.setRole("Customer");
+        return login;
+    }
+
+    login.setRole("Invalid");
+    return login;
+  }
+
+  private boolean checkAdmin(String username, String password) {
+    if (username.equals("admin") && password.equals("admin@123")) {
+        return true;
+    }
+
+    return false;
+  }
+}
---
+++ src/main/java/com/dbms/team15/models/Login.java
@@ -0,0 +1,37 @@
+package com.dbms.team15.models;
+
+public class Login {
+
+    private String username;
+    private String password;
+    private String role;
+
+    //Constructor
+    public Login(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+}
